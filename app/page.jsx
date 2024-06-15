883259e3bd68ffb57e4ed3c7a05e290037ba3994
coolboyalan <deepaky954000@gmail.com>
2024-06-15 23:54:09 +0530

    added metadata for homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
 import TabSelector from "@/components/TabSelector";
 
+export const metadata = {
+  title: 'Darkastic',
+  description:'New Quotes Everyday'
+}
+
 const Home = () => {
   return (
     <section className="bg-white h-screen px-4 text-black">
