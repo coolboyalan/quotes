292d4ef8cd9c9a877cd03aee8bfb85b02b963296
Alan <deepaky954000@gmail.com>
2024-07-04 15:33:25 +0530

    updated env

--- db/sequelize.js
+++ db/sequelize.js
@@ -4,7 +4,7 @@ const pg = require("pg");
 pg.defaults.parseInt8 = true;
 
 const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASS, {
-  host: "localhost",
+  host: process.env.HOST,
   dialect: "postgres", // Change this to your database dialect (e.g., postgres for PostgreSQL)
   dialectModule: pg,
 });
